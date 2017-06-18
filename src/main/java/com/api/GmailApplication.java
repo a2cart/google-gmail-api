34dab4f7973772852379e16dc05fedf9230e24ac
www.a2cart.com <balakumar-s@hcl.com>
2017-06-18 13:58:29 +0530

    Google Gmail API Using OAuth2

---
+++ src/main/java/com/api/GmailApplication.java
@@ -0,0 +1,13 @@
+package com.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmailApplication.class, args);
+	}
+
+}
